11cc2720d384ea008225933d08ff65ceebfeedf5
Martin Dummer <martin.dummer@gmx.net>
2012-06-19 10:01:18 +0200

    merge patch with changes for VDR >= 1.5.7 from Stone <syphyr@gmail.com>

--- undelete.c
+++ undelete.c
@@ -198,7 +198,11 @@ class cPluginUndelete : public cPlugin {
 private:
   // Add any member variables or functions you may need here.
   void FreeKeyNames(void);
+#if VDRVERSNUM >= 10507
+  char* OSDLanguage;
+#else
   int OSDLanguage;
+#endif
   void TestAndSetOSDLanguage(void);
   bool ProcessArg(int argc, char *argv[]);
 #ifdef UND_Debug
